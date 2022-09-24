9a0cc526c95fba7f989079cb0b5a14ffdc773ad8
LabhDeveloper <labh.k2003@gmail.com>
2022-09-24 15:03:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ app.post("/callback", (req, res) => {
            var dat = _results.TXNDATE;
 
            /* where it will come back after payment*/
-           res.redirect(`https://lens-lk-kart.netlify.app/viewBooking?status=${sat.split('_')[1]}&ORDERID=${_results.ORDERID}&date=${_results.TXNDATE.slice(0,10)}&bank=${_results.BANKNAME.split('%')[0]}`)
+           res.redirect(`http://localhost:3000/viewBooking?status=${sat.split('_')[1]}&ORDERID=${_results.ORDERID}&date=${_results.TXNDATE.slice(0,10)}&bank=${_results.BANKNAME.split('%')[0]}`)
            });
        });
 
