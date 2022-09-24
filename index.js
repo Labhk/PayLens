deadf9ad45ae04e121b337f2c886ddd220a516a5
LabhDeveloper <labh.k2003@gmail.com>
2022-09-24 15:40:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.custo
     params['CUST_ID'] = paymentDetails.customerId;
     params['TXN_AMOUNT'] = paymentDetails.amount;
     /* where is app is hosted (heroku url)*/
-    params['CALLBACK_URL'] = 'http://pay-lens.herokuapp.com/callback';
+    params['CALLBACK_URL'] = 'https://lens-lk-kart.netlify.app/';
     params['EMAIL'] = paymentDetails.customerEmail;
     params['MOBILE_NO'] = paymentDetails.customerPhone;
   
