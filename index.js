5468b8c6b59e0ffe6e9b243567f9ea5796ee8e79
LabhDeveloper <labh.k2003@gmail.com>
2022-09-26 17:12:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,12 +24,12 @@ app.post("/paynow", [parseUrl, parseJson], (req, res) => {
   var paymentDetails = {
     orderID: req.body.order_id,
     amount: req.body.cost,
-    customerId: req.body.name,
+    customerId: req.body.name.trim(),
     customerEmail: req.body.email,
     customerPhone: req.body.phone,
     customerBrand: req.body.brand_name
 }
-paymentDetails.customerId = paymentDetails.customerId.trim();
+;
 if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.customerEmail || !paymentDetails.customerPhone ) {
   res.status(400).send('Payment failed')
 } else {
